2e97d43729b866965604a02ac5a24a7e680ca1c9
Aydin <aydinschwa@gmail.com>
2022-04-04 20:55:55 -0700

    switch board and ship placing function to use numpy

--- Battleship/battleship_pg.py
+++ Battleship/battleship_pg.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import numpy as np
 import random
 import sys
 
@@ -7,7 +8,7 @@ SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 SCREEN = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-BOARD = [[0 for _ in range(10)] for _ in range(10)]
+BOARD = np.zeros([10, 10])
 SQUARE_SIZE = 45
 OFFSET_SIZE = 2
 WHITE = (255, 255, 255)
@@ -16,6 +17,7 @@ BLACK = (0, 0, 0)
 SHIP_SIZES = [6, 5, 3, 3, 2]
 
 
+# switching to using numpy here will probably help a lot
 def place_ships():
     for ship_size in SHIP_SIZES:
 
@@ -32,56 +34,32 @@ def place_ships():
 
             # select endpoint
             if const_axis == "row":
-                if direction == "up":
-                    end_row = start_row
+                if direction == "up" and start_col - ship_size >= 0:
+                    end_row = start_row + 1
                     end_col = start_col - ship_size
-                elif direction == "down":
-                    end_row = start_row
+                    start_col, end_col = end_col, start_col
+                elif direction == "down" and start_col + ship_size <= 9:
+                    end_row = start_row + 1
                     end_col = start_col + ship_size
+                else:
+                    continue
 
             elif const_axis == "col":
-                if direction == "up":
+                if direction == "up" and start_row - ship_size >= 0:
                     end_row = start_row - ship_size
-                    end_col = start_col
-                elif direction == "down":
+                    start_row, end_row = end_row, start_row
+                    end_col = start_col + 1
+                elif direction == "down" and start_row + ship_size <= 9:
                     end_row = start_row + ship_size
-                    end_col = start_col
-
-            # try again if end indices are out of bounds
-            if (end_row < 0 or end_row > 9) or (end_col < 0 or end_col > 9):
-                continue
-
-            # get smallest idx placed first
-            if start_row > end_row:
-                start_row, end_row = end_row, start_row
-            if start_col > end_col:
-                start_col, end_col = end_col, start_col
+                    end_col = start_col + 1
+                else:
+                    continue
 
             # check that all spaces that we want to insert into are clear
-            valid = True
-            if const_axis == "col":
-                for i in range(start_row, end_row):
-                    if BOARD[i][end_col] != 0:
-                        valid = False
-                        break
-
-            elif const_axis == "row":
-                for i in range(start_col, end_col):
-                    if BOARD[end_row][i] != 0:
-                        valid = False
-                        break
-
-            if not valid:
+            if np.all(BOARD[start_row:end_row, start_col:end_col] == 0):
+                BOARD[start_row:end_row, start_col:end_col] = 1
+            else:
                 continue
-
-            if const_axis == "col":
-                for i in range(start_row, end_row):
-                    BOARD[i][end_col] = 1
-
-            elif const_axis == "row":
-                for i in range(start_col, end_col):
-                    BOARD[end_row][i] = 1
-
             break
 
 
@@ -93,15 +71,15 @@ def draw_board(board):
             if val == 0:
                 pg.draw.rect(SCREEN, WHITE, pg.Rect(board_x, board_y, SQUARE_SIZE, SQUARE_SIZE))
             elif val == 1:
-                pg.draw.rect(SCREEN, WHITE, pg.Rect(board_x, board_y, SQUARE_SIZE, SQUARE_SIZE))
-                pg.draw.line(SCREEN, BLACK,
-                             (board_x, board_y),
-                             (board_x + SQUARE_SIZE, board_y + SQUARE_SIZE),
-                             width=5)
-                pg.draw.line(SCREEN, BLACK,
-                             (board_x, board_y + SQUARE_SIZE),
-                             (board_x + SQUARE_SIZE, board_y),
-                             width=5)
+                pg.draw.rect(SCREEN, (111, 111, 111), pg.Rect(board_x, board_y, SQUARE_SIZE, SQUARE_SIZE))
+                # pg.draw.line(SCREEN, BLACK,
+                #              (board_x, board_y),
+                #              (board_x + SQUARE_SIZE, board_y + SQUARE_SIZE),
+                #              width=5)
+                # pg.draw.line(SCREEN, BLACK,
+                #              (board_x, board_y + SQUARE_SIZE),
+                #              (board_x + SQUARE_SIZE, board_y),
+                #              width=5)
 
             board_x += SQUARE_SIZE + OFFSET_SIZE
 
